ea3f8a6d2b8b034d40eba4f7da10ad4b2b81bea8
Morten Hau Lyng <mhl@it-minds.dk>
2020-10-15 12:08:35 +0200

    Initial extension bundle

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace LordLyngLib.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return str.IsNullOrEmpty();
+        }
+
+        public static bool NotNullOrEmpty(this string str)
+        {
+            return !str.IsNullOrEmpty();
+        }
+
+        public static bool IsNullOrWhitespace(this string str)
+        {
+            return str.IsNullOrWhitespace();
+        }
+
+        public static bool NotNullOrWhitespace(this string str)
+        {
+            return !str.IsNullOrWhitespace();
+        }
+
+        public static bool IsValidGuid(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            var canParse = Guid.TryParse(str, out var result);
+
+            return canParse && result != default;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# LordLyngLib
+My first class library!
+
+It contains a collection of useful extensions and snippets for C# development.
